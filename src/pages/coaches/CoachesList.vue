2d094d9bcb19834ed4f0fa25f4b2d0da26d4df98
Salokhiddinov <nomonsalakhiddinov2002@gmail.com>
2022-04-29 22:16:50 +0500

    feat: add CoachItem component

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,27 +1,45 @@
 <template>
-    <section>
-        FILTER
-    </section>
-    <section>
-        <div class="controls">
-            <button>Refresh</button>
-            <router-link to="/register">Register as Coach</router-link>
-        </div>
-        <ul>
-            <li v-for='coach in filteredCoaches' :key="coach.id">
-                {{ coach.firstName }} {{ coach.lastName }}
-            </li>
-        </ul>
-        LIST OF COACHES
-    </section>
+  <section>FILTER</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+    <ul>
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      ></coach-item>
+    </ul>
+  </section>
 </template>
 
 <script>
+import CoachItem from '@/components/coaches/CoachItem.vue';
 export default {
-    computed: {
-        filteredCoaches() {
-            return this.$store.getters['coaches/coaches'];
-        }
-    }
-}
+  components: { CoachItem },
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+  },
+};
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
