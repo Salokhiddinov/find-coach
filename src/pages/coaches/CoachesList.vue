0cb3738696580ee87fb35071515a9458212c65dc
Salokhiddinov <nomonsalakhiddinov2002@gmail.com>
2022-04-29 15:42:21 +0500

    feat: add vuex for coaches

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -7,7 +7,21 @@
             <button>Refresh</button>
             <router-link to="/register">Register as Coach</router-link>
         </div>
-        <ul></ul>
+        <ul>
+            <li v-for='coach in filteredCoaches' :key="coach.id">
+                {{ coach.firstName }} {{ coach.lastName }}
+            </li>
+        </ul>
         LIST OF COACHES
     </section>
 </template>
+
+<script>
+export default {
+    computed: {
+        filteredCoaches() {
+            return this.$store.getters['coaches/coaches'];
+        }
+    }
+}
+</script>
