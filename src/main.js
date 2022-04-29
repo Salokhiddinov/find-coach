9fcf5e6b5cd5abb812ec83d7687bab562a117453
Salokhiddinov <nomonsalakhiddinov2002@gmail.com>
2022-04-29 22:57:03 +0500

    feat: add UI components + styles

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import { createApp } from 'vue';
 import router from '@/router';
 import  App from '@/App.vue';
+import BaseCard from '@/components/UI/BaseCard.vue';
+import BaseButton from '@/components/UI/BaseButton.vue';
+import BaseBadge from '@/components/UI/BaseBadge.vue';
+
+
 import store from './store//index.js';
 
 const app = createApp(App);
+
+
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
 
 
 
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,21 +1,23 @@
 <template>
   <section>FILTER</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul>
-      <coach-item
-        v-for="coach in filteredCoaches"
-        :key="coach.id"
-        :id="coach.id"
-        :first-name="coach.firstName"
-        :last-name="coach.lastName"
-        :rate="coach.hourlyRate"
-        :areas="coach.areas"
-      ></coach-item>
-    </ul>
+    <base-card>
+      <div class="controls">
+        <base-button mode='outline'>Refresh</base-button>
+        <base-button link to="/register">Register as Coach</base-button>
+      </div>
+      <ul>
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+      </ul>
+    </base-card>
   </section>
 </template>
 
